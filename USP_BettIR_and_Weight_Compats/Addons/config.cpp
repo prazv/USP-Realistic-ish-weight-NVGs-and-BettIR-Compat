b49eb8958ba4151f77c3e941f19b1de0df95b8bd
prazv <64920464+prazv@users.noreply.github.com>
2025-07-30 17:50:11 +0300

    Add files via upload

---
+++ USP_BettIR_and_Weight_Compats/Addons/config.cpp
@@ -0,0 +1,544 @@
+class CfgPatches
+{
+	class BettIR_ace_compat
+	{
+		requiredaddons[]=
+		{
+			"BettIR_Core",
+			"USP_Gear_Nvg"
+		};
+		requiredversion=1;
+		units[]={};
+		weapons[]={};
+		name="USP BettIR Compat";
+		author="prazv";
+	};
+};
+class BettIR_Config
+{
+	class CompatibleNightvisionGoggles
+	{
+		class __base_NVG;
+        class USP_NSEAS: __base_NVG
+		{
+		};
+        class USP_GPNVG18_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_BLK2: __base_NVG
+		{
+		};
+        class USP_GPNVG18_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_TAN2: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_BLK2: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_TAN2: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB2_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB2_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB2_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB2_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB2_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB2_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB2_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB2_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB3_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB3_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB3_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB3_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB3_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_CB3_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB3_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_CB3_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_GM_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_GM_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_GM_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_GM_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_GM_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_GM_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_GM_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_GM_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_NSEAS: __base_NVG
+		{
+		};
+        class USP_NSEAS: __base_NVG
+		{
+		};
+        class USP_GPNVG18_TAR_BLK2: __base_NVG
+		{
+		};
+        class USP_GPNVG18_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_TAR_TAN2: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_GPNVG18_WP_TAR_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS14: __base_NVG
+		{
+		};
+        class USP_PVS14_WP: __base_NVG
+		{
+		};
+        class USP_PVS14_TAR: __base_NVG
+		{
+		};
+        class USP_PVS14_WP_TAR: __base_NVG
+		{
+		};
+        class USP_PVS15: __base_NVG
+		{
+		};
+        class USP_PVS15_WP: __base_NVG
+		{
+		};
+        class USP_PVS15_TAR: __base_NVG
+		{
+		};
+        class USP_PVS15_WP_TAR: __base_NVG
+		{
+		};
+        class USP_NSEAS: __base_NVG
+		{
+		};
+        class USP_PVS31_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_HIGH_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_HIGH_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_HIGH_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_HIGH_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_HIGH_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_HIGH_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_HIGH_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_HIGH_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_LOW_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_LOW_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_LOW_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_LOW_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_LOW_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_LOW_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_LOW_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_LOW_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_MID_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_MID_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_MID_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_MID_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_MID_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_MID_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_MID_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_MID_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_MONOL_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_MONOL_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_MONOR_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_MONOR_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_HIGH: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_HIGH_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_HIGH_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_HIGH_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_HIGH_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_HIGH_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_HIGH_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_HIGH_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_LOW: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_LOW_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_LOW_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_LOW_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_LOW_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_LOW_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_LOW_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_LOW_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_MID: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_MID_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_MID_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_TAR_MID_TAN2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_MID_BLK: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_MID_BLK2: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_MID_TAN: __base_NVG
+		{
+		};
+        class USP_PVS31_WP_TAR_MID_TAN2: __base_NVG
+		{
+		};
+    };
+};
+class CfgWeapons
+{
+    class NVGoggles;
+	
+    class USP_NSEAS: NVGoggles
+    {
+        mass = 16;
+    };
+   class USP_GPNVG18_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_CB2_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_CB_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_CB_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_CB_TAR_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_CB2_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_CB2_TAR_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_CB3_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_CB3_TAR_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_GM_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_CB_TAR_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_CB2_TAR_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_CB3_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_CB3_TAR_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_GPNVG18_WP_GM_BLK: NVGoggles
+    {
+        mass = 24;
+    };
+    class USP_PVS14: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS14_WP: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS15: NVGoggles
+    {
+        mass = 20;
+    };
+    class USP_PVS15_WP: NVGoggles
+    {
+        mass = 20;
+    };
+    class USP_PVS31_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_HIGH_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_LOW_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_MID_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_MONOL_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_MONOR_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_WP_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_WP_HIGH_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_WP_LOW_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_WP_MID_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_WP_MONOL_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+    class USP_PVS31_WP_MONOR_BLK: NVGoggles
+    {
+        mass = 16;
+    };
+};
---
+++ USP_BettIR_and_Weight_Compats/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 0;
+name = "USP Realistic-ish weight and BettIR Compat";
+timestamp = 5250580846512403948;
